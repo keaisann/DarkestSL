c6ace3b1e2f0bfc8855356a8af0a8fb35e242141
keaisann <keaisann@qq.com>
2025-09-05 08:43:14 +0800

    refactor: 重构项目并优化代码
    - 更新 .gitignore 文件，添加 Backups 文件夹到忽略列表
    - 删除 .idea/.gitignore 文件
    - 重构 DarkestSL.py:
      - 导入模块优化，使用 from 模块 import 方式
      - 重写 Application 类，改进代码结构和可读性
      - 优化 UI 布局和组件命名
      - 添加 DPI 感知支持
    - 重构 game_save_mgr.py:
      - 优化 GameSaveMgr 类的方法实现
      - 改进代码格式和变量命名
      - 更新文档注释

--- DarkestSL.py
+++ DarkestSL.py
@@ -1,76 +1,107 @@
+import ctypes
 import base64
 import os
 
 from icon import Icon
-from tkinter import *  # 注意模块导入方式，否则代码会有差别
-from tkinter import filedialog
-from tkinter import messagebox
+from tkinter import (
+    BOTH,
+    LEFT,
+    NW,
+    YES,
+    BooleanVar,
+    Button,
+    Checkbutton,
+    Entry,
+    Frame,
+    Tk,
+    filedialog,
+    messagebox,
+)
 
 from game_save_mgr import game
 
 
 class Application(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master)
+    def __init__(self, *args: ..., **kwargs: ...):
+        super().__init__(*args, **kwargs)
         self.pack()
-        self.restart_option = BooleanVar()
-        self.save_file_name_fmt = ''
-        self.file_name_entry = Entry(state='disabled')
-        self.save_option = BooleanVar()
+        self.option_restart = BooleanVar()
+        self.option_save = BooleanVar()
+        self.file_name = Entry(state="readonly")
         self.create_widget()
 
     def create_widget(self):
         fm_top = Frame(self)
-        Button(fm_top, text='保存', command=self.save_game, height=2, width=20).pack(side=LEFT, padx=5, pady=5)
-        Button(fm_top, text='读取', command=self.restore_game, height=2, width=20).pack(side=LEFT, padx=5, pady=5)
+        Button(fm_top, text="保存", command=self.save_game, height=3, width=20).pack(
+            side=LEFT, padx=5, pady=5
+        )
+        Button(fm_top, text="读取", command=self.restore_game, height=3, width=20).pack(
+            side=LEFT, padx=5, pady=5
+        )
         fm_top.pack(anchor=NW)
 
         fm_down = Frame(self)
-        Checkbutton(fm_down, text='启动游戏', command=self.get_chk, variable=self.restart_option).pack(side=LEFT)
-        Checkbutton(fm_down, text='自定义存档名', command=self.get_chk, variable=self.save_option).pack(side=LEFT)
+        Checkbutton(
+            fm_down, text="启动游戏", command=self.get_chk, variable=self.option_restart
+        ).pack(side=LEFT)
+        Checkbutton(
+            fm_down,
+            text="自定义存档名",
+            command=self.get_chk,
+            variable=self.option_save,
+        ).pack(side=LEFT)
         fm_down.pack(anchor=NW, padx=5, pady=5, expand=YES, fill=BOTH)
-        self.file_name_entry.pack(padx=5, pady=5, expand=YES, fill=BOTH)
+        self.file_name.pack(padx=5, pady=5, expand=YES, fill=BOTH)
 
     def get_chk(self):
-        if self.save_option.get():
-            self.file_name_entry['state'] = 'normal'
+        if self.option_save.get():
+            self.file_name["state"] = "normal"
         else:
-            self.file_name_entry.delete(0, END)
-            self.file_name_entry['state'] = 'disabled'
+            self.file_name["state"] = "readonly"
 
     def save_game(self):
-        exit_code, info = game.game_save(self.file_name_entry.get(), 0)
-        title = '操作成功'
+        exit_code, info = game.save_game(
+            self.file_name.get() if self.option_save.get() else "", False
+        )
+        title = "操作成功"
         if exit_code:
-            title = '操作失败'
+            title = "操作失败"
         else:
-            info = '文件已保存至%s' % game.backup_file
+            info = "文件已保存至%s" % game.backup_file
         messagebox.showinfo(title, info)
 
     def restore_game(self):
-        backup_file = filedialog.askopenfilename(initialdir=game.backup_folder, title="Select file",
-                                                 filetypes=(("7zip files", "*.7z"), ("all files", "*.*")))
-        print(backup_file)
+        backup_file = filedialog.askopenfilename(
+            initialdir=game.backup_folder,
+            title="Select file",
+            filetypes=(("7zip files", "*.7z"), ("all files", "*.*")),
+        )
 
-        title = '操作成功'
-        if backup_file != '':
-            exit_code, info = game.game_restore(backup_file, self.restart_option.get())
+        title = "操作成功"
+        if backup_file != "":
+            exit_code, info = game.game_restore(backup_file, self.option_restart.get())
             if not exit_code:
-                title = '操作失败'
-            if self.restart_option.get() == 0 or exit_code != 0:
+                title = "操作失败"
+            if self.option_restart.get() == 0 or exit_code != 0:
                 messagebox.showinfo(title, info)
             else:
                 pass
 
 
 root = Tk()
-root.title('DarkestSL')
-root.resizable(0, 0)
+root.title("DarkestSL")
+root.resizable(False, False)
 
-with open('tmp.ico', 'wb') as tmp:
+
+with open("tmp.ico", "wb") as tmp:
     tmp.write(base64.b64decode(Icon().img))
-root.iconbitmap('tmp.ico')
-os.remove('tmp.ico')
+root.iconbitmap("tmp.ico")  # type:ignore
+os.remove("tmp.ico")
+
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
+ScaleFactor = ctypes.windll.shcore.GetScaleFactorForDevice(0)
+root.tk.call("tk", "scaling", ScaleFactor / 75)
+
 
 app = Application(root)
 app.mainloop()
--- game_save_mgr.py
+++ game_save_mgr.py
@@ -9,16 +9,18 @@ from configparser import ConfigParser, NoOptionError
 
 
 def run_cmd(cmd):
-    sub_p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=-1)
+    sub_p = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=-1
+    )
     sub_p.wait()
     stream_stdout = io.TextIOWrapper(sub_p.stdout).read()
     stream_stderr = io.TextIOWrapper(sub_p.stderr).read()
     exit_code = 0
-    info = ''
-    if stream_stdout != '':
+    info = ""
+    if stream_stdout != "":
         print(stream_stdout)
         info = stream_stdout
-    if stream_stderr != '':
+    if stream_stderr != "":
         print(stream_stderr)
         info = stream_stderr
         exit_code = 1
@@ -26,46 +28,59 @@ def run_cmd(cmd):
 
 
 class GameSaveMgr(object):
-    def __init__(self, program_name, p7zip_path, save_folder, backup_folder, rungame_url):
+    def __init__(
+        self, program_name, p7zip_path, save_folder, backup_folder, rungame_url
+    ):
         self.program_name = program_name
         self.rungame_url = rungame_url
         self.p7zip_path = p7zip_path
         self.save_folder = save_folder
         self.backup_folder = backup_folder
-        self.backup_file = ''
+        self.backup_file = ""
         self.sleep_sec = 0
 
-    def game_save(self, filename='', overwrite=1):
+    def save_game(self, filename: str = "", overwrite: bool = True):
         """文件名默认为时间戳，若指定非空文件名则默认覆盖，不覆盖则文件名追加时间戳。
         参数:
             filename: 指定的文件名
             overwrite: 指定是否覆盖
         """
 
-        timestamp = datetime.now().strftime('%Y-%m-%d_%H_%M_%S_%f')
-        if filename == '':
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H_%M_%S_%f")
+        if filename == "":
             self.backup_file = os.path.join(self.backup_folder, timestamp)
-        elif overwrite == 1:
+        elif overwrite:
             self.backup_file = os.path.join(self.backup_folder, filename)
         else:
-            self.backup_file = os.path.join(self.backup_folder, filename + '_' + timestamp)
+            self.backup_file = os.path.join(
+                self.backup_folder, filename + "_" + timestamp
+            )
 
-        zip_format = '7z'  # 注意：经测试，不支持gzip，bzip2。
+        zip_format = "7z"  # 注意：经测试，不支持gzip，bzip2。
 
         # 拼接并执行命令，删除同名文件后更新压缩包。
-        cmd = '\"%s\" a -t%s \"%s\" \"%s\"' % (self.p7zip_path, zip_format, self.backup_file, self.save_folder)
+        cmd = '"%s" a -t%s "%s" "%s"' % (
+            self.p7zip_path,
+            zip_format,
+            self.backup_file,
+            self.save_folder,
+        )
         try:
-            os.remove(self.backup_file + '.%s' % zip_format)
-            print('将覆盖原文件')
+            os.remove(self.backup_file + ".%s" % zip_format)
+            print("将覆盖原文件")
         except FileNotFoundError:
-            print('将创建新文件')
+            print("将创建新文件")
         finally:
             return run_cmd(cmd)
 
     def game_load(self, backup_file_path):
         """从备份目录指定文件，恢复至存档目录，并且静默覆盖"""
-        restore_dir = os.path.join(self.save_folder, '..')
-        cmd = "\"%s\" x \"%s\" -o\"%s\" -aoa" % (self.p7zip_path, backup_file_path, restore_dir)
+        restore_dir = os.path.join(self.save_folder, "..")
+        cmd = '"%s" x "%s" -o"%s" -aoa' % (
+            self.p7zip_path,
+            backup_file_path,
+            restore_dir,
+        )
         return run_cmd(cmd)
 
     def backup_list(self):
@@ -73,7 +88,9 @@ class GameSaveMgr(object):
         file_list = os.listdir(self.backup_folder)
         backup_list = []
         for file in file_list:
-            backup_list.append([file, os.path.getctime(self.backup_folder + '\\' + file)])
+            backup_list.append(
+                [file, os.path.getctime(self.backup_folder + "\\" + file)]
+            )
         backup_list = sorted(backup_list, key=lambda time: time[1], reverse=1)
         return backup_list
 
@@ -83,7 +100,7 @@ class GameSaveMgr(object):
         返回值格式：(状态码, 状态信息)
         """
         # 当指定游戏进程不存在时，返回值为0。
-        cmd = 'taskkill /F /IM %s' % self.program_name
+        cmd = "taskkill /F /IM %s" % self.program_name
         exit_code, info = run_cmd(cmd)
         if info == '错误: 没有找到进程 "%s"。\n' % self.program_name:
             exit_code = 0
@@ -104,23 +121,20 @@ class GameSaveMgr(object):
 
 
 cfg = ConfigParser()
-cfg.read(os.path.join(os.getcwd(), 'config.ini'))
+cfg.read(os.path.join(os.getcwd(), "config.ini"))
 
-p_name = cfg.get('Game', 'program_name')
-p7z_path = cfg.get('Game', 'p7zip_path')
-sv_folder = cfg.get('Game', 'save_folder')
-rg_url = cfg.get('Game', 'rungame_url')
-bak_folder = cfg.get('Game', 'backup_folder')
+p_name = cfg.get("Game", "program_name")
+p7z_path = cfg.get("Game", "p7zip_path")
+sv_folder = cfg.get("Game", "save_folder")
+rg_url = cfg.get("Game", "rungame_url")
+bak_folder = cfg.get("Game", "backup_folder")
 
-game = GameSaveMgr(p_name, p7z_path,
-                   sv_folder,
-                   bak_folder,
-                   rg_url)
+game = GameSaveMgr(p_name, p7z_path, sv_folder, bak_folder, rg_url)
 
 try:
-    game.sleep_sec = int(cfg.get('Game', 'sleep_sec'))
+    game.sleep_sec = int(cfg.get("Game", "sleep_sec"))
 except NoOptionError:
     pass
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
